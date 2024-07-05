2f59c2dd2f7505d9d2eb1563e5551d0bb2a52356
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-05 18:05:34 +0100

    added the Bintree.h and defenition of classes Bintree, and Tree

---
+++ cw38_BinTree/BinTree.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+namespace BinTree {
+	template <typename T>
+	class Node {
+	public:
+		T info;
+		Node* left;
+		Node* right;
+
+		Node(T value) {
+			info = value;
+			left = right = nullptr;
+		}
+
+		void showNode() {
+			cout << "INFO: " << info << endl;
+			cout << "\tLeft: " << left << endl;
+			cout << "\tRight: " << right << endl << endl;
+		}
+	};
+
+	template <typename U>
+	class Tree {
+		Node<U>* root;
+	public:
+		Tree() {
+			root = nullptr;
+		}
+		~Tree() {
+
+		}
+
+		void insert(U value) {
+			Node<U>* el = new Node<U>(value);
+
+			if (root == nullptr) {
+				root = el;
+			}
+			else {
+				Node<U>* p = root;
+				Node<U>* place = root;
+				//Шукаємо місце для створеного вузла
+				while (p != nullptr)
+				{
+					/*if (el->info < p->info)
+						p = p->left;
+					else
+						p = p->right;*/
+
+					place = p;
+					p = (el->info < p->info) ? p->left : p->right;
+				}
+
+				//Привязка нового вузла
+				if (el->info < place->info)
+					place->left = el;
+				else
+					place->right = el;
+			}
+		}
+	};
+
+
+}
---
+++ cw38_BinTree/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "BinTree.h"
+
+int main() {
+    
+	BinTree::Tree<int> bin;
+	bin.insert(55);
+	bin.insert(11);
+	bin.insert(19);
+	bin.insert(666);
+	bin.insert(999);
+	bin.insert(777);
+	bin.insert(34);
+	bin.insert(76);
+	bin.insert(77);
+
+	cout << endl;
+
+}
